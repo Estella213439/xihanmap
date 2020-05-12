ee61315c0e58962bb30c742df5bedd64d3ab237c
Jiahua Chen <estella213439@gmail.com>
2020-05-12 18:55:53 -0400

    subset timeline

--- main.js
+++ main.js
@@ -1,140 +1,177 @@
+//IMPORT TILES
+
 var myIcon;
+var mapboxAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+var mURL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXN0ZWxsYTIxMzQzOSIsImEiOiJjanJ1eTRndHgzMDY2M3lsYXMwaHVqNGVuIn0.-NQ2jPS_wCAxQ2bCVbsabA';
+var satellite = L.tileLayer(mURL, {id: 'mapbox/satellite-v9', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution,}),
+    streets   = L.tileLayer(mURL, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution}),
+    topography = L.tileLayer(mURL, {id: 'estella213439/ck76lx6r4010e1iqztwsspjl5', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution});
 
-var mymap = L.map('mapid', {
-  zoom: 6,
-  center: new L.latLng(35, 115)
-}),
-geojsonOpts = {
-  style: function (feature) {
-    return {color: feature.properties.color};
-  },
 
-  // onEachFeature: function (feature, layer) {
-    
-  // },
-  // pointToLayer: function(feature, latlng) {
-  //   return L.marker(latlng,{icon: myIcon})
-  // }
-}
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'estella213439/ck76lx6r4010e1iqztwsspjl5',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZXN0ZWxsYTIxMzQzOSIsImEiOiJjanJ1eTRndHgzMDY2M3lsYXMwaHVqNGVuIn0.-NQ2jPS_wCAxQ2bCVbsabA'
-}).addTo(mymap);
-
-var myIconDomain = L.icon({
-  iconUrl: "/data/china.svg",
-  iconSize: [30, 30],
-  iconAnchor: [9, 21],
-  popupAnchor: [0, -14]
-});
-
-var myIconTomb = L.icon({
-  iconUrl: '/data/tomb.svg',
-  iconSize: [30, 30],
-  iconAnchor: [9, 21],
-  popupAnchor: [0, -14]
-});
-
-var myIconOthers = L.icon({
-  iconUrl: '/data/origin.svg',
-  iconSize: [30, 30],
-  iconAnchor: [9, 21],
-  popupAnchor: [0, -14]
-});
-
-// var geoJsonLayer = L.geoJson(markers, geojsonOpts);
-
-// var cluster = L.markerClusterGroup();
-// cluster.addLayer(geoJsonLayer);
-// mymap.addLayer(cluster);
-
-// var controlSearch_domain = new L.Control.Search({
-//   position:'topleft',    // where do you want the search bar?
-//   layer: cluster,  // name of the layer
-//   initial: false,
-//   propertyName: 'domain_en',
-//   zoom: 12,        // set zoom to found location when searched
-//   marker: false,
-//   textPlaceholder: 'search...',
-//   minLength: 0, // placeholder while nothing is searched
-// });
-
-// mymap.addControl(controlSearch_domain);
-
-// var controlSearch_name = new L.Control.Search({
-//   position:'topright',    // where do you want the search bar?
-//   layer: cluster,  // name of the layer
-//   initial: false,
-//   propertyName: 'name_en',
-//   zoom: 12,        // set zoom to found location when searched
-//   marker: false,
-//   textPlaceholder: 'search...', // placeholder while nothing is searched
-//   minLength: 0 // Show full list when no text is typed,
-//  });
-
-// mymap.addControl(controlSearch_name);
-
-// var c = [];
-// var line;
-
-// controlSearch_name.on('search:locationfound', function(e) {
-//   var marker =  e.layer;
-//   mymap.addLayer(marker);
-//   mymap.removeLayer(cluster)
-//   console.log(marker);
-//   var x = marker._latlng.lat;
-//   var y = marker._latlng.lng;
-//   c.push([x, y]);``
-
-// })
-// .on('search:collapsed', function(e) {
-//   //unselect all markers
-//   // mymap.removeLayer(marker)
-//   mymap.setView([35, 115], 6);
-//   mymap.addLayer(cluster);
-//   if (e.layer != undefined) {
-//     mymap.removeLayer(e.layer);
-//   };
-//   mymap.removeLayer(geoJsonLayer)
-// });
+//TIME SLIDER
 
 var slider = L.timelineSliderControl({
   formatOutput: function(date){
-    return moment(date).format("YYYY-MM-DD");
+    return moment(date).format("YYYY");
   }
 });
-mymap.addControl(slider);
 
-var pointTimeline = L.timeline(markers1, {
-  
+
+var domainPoint = new L.timeline(markers1, {
   onEachFeature: function(feature, layer) {
     layer.bindTooltip(popup(feature));
-  }
-});
-pointTimeline.addTo(mymap);
-slider.addTimelines(pointTimeline);
+    },
+  filter: function(feature, layer) {
+    if (feature.properties.type == "domain"){
+      return true;
+    }
+    else{
+      return false;
+    } 
+  }}
+);
+var originPoint = new L.timeline(markers1, {
+  onEachFeature: function(feature, layer) {
+    layer.bindTooltip(popup(feature));
+    },
+  filter: function(feature, layer) {
+    if (feature.properties.type == "origin"){
+      return true;
+    }
+    else{
+      return false;
+    } 
+  }});
+var tombPoint = new L.timeline(markers1, {
+  onEachFeature: function(feature, layer) {
+    layer.bindTooltip(popup(feature));
+    },
+  filter: function(feature, layer) {
+    if (feature.properties.type == "tomb"){
+      return true;
+    }
+    else{
+      return false;
+    } 
+  }});
+
+  //CREATE MAP
+  var mymap = L.map('mapid', {
+    zoom: 6,
+    center: new L.latLng(35, 115),
+    layers: [topography]
+  });
+
+  mymap.addControl(slider);
+// var pointTimeline = L.timeline(markers1, {
+//   onEachFeature: function(feature, layer) {
+//   layer.bindTooltip(popup(feature));
+//   }
+//     // pointToLayer: function(feature, latlng) {
+//   //   return L.marker(latlng,{icon: myIcon})
+// });
+slider.addTimelines(domainPoint,originPoint,tombPoint);
+
+//POPUP
 
 function popup(feature){
   var popup;
   if (feature.properties.type == "domain") {
     popup = "<strong style='color: #84b819'>" +  feature.properties.domain_en +' '+ feature.properties.domain_ch +" </strong><br>" +'<b>Name:</b> '+ feature.properties.name_en + ' '+feature.properties.name_ch + '<br/><b>Regnal Year: </b>'+feature.properties.start_ori + ' B.C.';
-    myIcon = myIconDomain;
+    // myIcon = myIconDomain;
   }
   else if (feature.properties.type == "tomb") {
     popup = "<strong style='color: #84b819'>" +  feature.properties.domain_en +' '+ feature.properties.domain_ch + "</strong><br>" +'<b>Name:</b> '+ feature.properties.name_en + ' '+feature.properties.name_ch + '<br/><b>Regnal Year: </b>'+feature.properties.start_ori +' B.C.';
-    myIcon = myIconTomb;
+    // myIcon = myIconTomb;
   }
-  else {
-    myIcon = myIconOthers;
-    var popup = "<strong style='color: #84b819'>" +  feature.properties.domain_en +' '+ feature.properties.domain_ch + " (Place of Origin)</strong><br>" +'<b>Name:</b> '+ feature.properties.name_en + ' '+feature.properties.name_ch+ '<br/><b>Regnal Year: </b>'+feature.properties.start_ori +' B.C.';
+  else if (feature.properties.type == "origin"){
+    // myIcon = myIconOthers;
+    popup = "<strong style='color: #84b819'>" +  feature.properties.domain_en +' '+ feature.properties.domain_ch + " (Place of Origin)</strong><br>" +'<b>Name:</b> '+ feature.properties.name_en + ' '+feature.properties.name_ch+ '<br/><b>Regnal Year: </b>'+feature.properties.start_ori +' B.C.';
+  }
+  else if (feature.properties.Type == "County"){
+    popup = "<strong style='color: #84b819'>" +  feature.properties.Name_en +' '+ feature.properties.Name + " (Place of Origin)</strong><br>" +'<b>Name:</b> '+ feature.properties.name_en + ' '+feature.properties.name_ch+ '<br/><b>Regnal Year: </b>'+feature.properties.start_ori +' B.C.';
   }
   return popup;
 }
+
+
+//DOMAIN POLYGON
+
+L.geoJSON(polygon2,{
+  onEachFeature: function(feature, layer) {
+    layer.bindTooltip("<style='color: #84b819'>" +  feature.properties.Name_en +' '+ feature.properties.Name +"</style='color: #84b819'>")
+    },
+  style: function(feature) {
+    switch (feature.properties.Type) {
+        case 'County': return {color: "#ff0000"};
+        case 'Domain':   return {color: "#0000ff"};
+    }
+  }}).addTo(mymap);
+
+
+//ALL POINTS
+
+var geoJsonLayer = L.geoJSON(markers1,{
+  onEachFeature: function(feature, layer) {
+        layer.bindPopup(popup(feature))
+        ;
+  }
+    // pointToLayer: function(feature, latlng) {
+  //   return L.marker(latlng,{icon: myIcon})
+})
+var cluster = L.markerClusterGroup();
+cluster.addLayer(geoJsonLayer);
+
+//SEARCH BAR
+
+var controlSearch_domain = new L.Control.Search({
+  position:'topleft',    // where do you want the search bar?
+  layer: cluster,  // name of the layer
+  initial: false,
+  propertyName: 'domain_en',
+  zoom: 12,        // set zoom to found location when searched
+  marker: false,
+  textPlaceholder: 'search...',
+  minLength: 0, // placeholder while nothing is searched
+});
+mymap.addControl(controlSearch_domain);
+
+var controlSearch_name = new L.Control.Search({
+  position:'topright',    // where do you want the search bar?
+  layer: cluster,  // name of the layer
+  initial: false,
+  propertyName: 'name_en',
+  zoom: 12,        // set zoom to found location when searched
+  marker: false,
+  textPlaceholder: 'search...', // placeholder while nothing is searched
+  minLength: 0, // Show full list when no text is typed,
+ });
+
+mymap.addControl(controlSearch_name);
+
+// var c = [];
+// var line;
+
+controlSearch_name.on('search:locationfound', function(e) {
+  var marker =  e.layer;
+  marker.setIcon(myIconDomain)
+  // mymap.addLayer(marker);
+  // mymap.removeLayer(cluster)
+  // var x = marker._latlng.lat;
+  // var y = marker._latlng.lng;
+  // c.push([x, y]);
+})
+.on('search:collapsed', function(e) {
+  var marker =  e.layer;
+  marker.setIcon(L.Icon.Default)
+  // e.target.setIcon(new myIconDomain);
+});
+
+controlSearch_domain.on('search:locationfound', function(e) {
+  var marker =  e.layer;
+  marker.setIcon(myIconDomain)
+})
 
 // function link(c) {
 //   line = L.polyline(c).addTo(mymap)
@@ -145,8 +182,46 @@ function popup(feature){
 // }).addTo(mymap);
 
 
+// TOGGOLE LAYERS
+
+var baseLayers = {
+  "Satellite": satellite,
+  "Streets": streets,
+  "Topography":topography
+};
+var overlayMaps = {
+  // "All Points": cluster,
+  // "timeline":pointTimeline,
+  "All Points(Search)":cluster,
+  "Domain(Timeline)":domainPoint,
+  "Origin(Timeline)":originPoint,
+  "Tomb(Timeline)":tombPoint
+};
+
+L.control.layers(baseLayers, overlayMaps).addTo(mymap);
 
 
 
 
 
+
+var myIconDomain = L.icon({
+  iconUrl: "/data/china.svg",
+  iconSize: [30, 30],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+});
+
+// // var myIconTomb = L.icon({
+// //   iconUrl: '/data/tomb.svg',
+// //   iconSize: [30, 30],
+// //   iconAnchor: [9, 21],
+// //   popupAnchor: [0, -14]
+// // });
+
+// // var myIconOthers = L.icon({
+// //   iconUrl: '/data/origin.svg',
+// //   iconSize: [30, 30],
+// //   iconAnchor: [9, 21],
+// //   popupAnchor: [0, -14]
+// // });
